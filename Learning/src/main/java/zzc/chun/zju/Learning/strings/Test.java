2f3b41e0bcb90d4156355b35a7986ee1aa43cfd0
xiaochunyn <1481839020@qq.com>
2016-09-18 22:01:29 +0800

    add Test.java

---
+++ Learning/src/main/java/zzc/chun/zju/Learning/strings/Test.java
@@ -0,0 +1,28 @@
+package zzc.chun.zju.Learning.strings;
+
+/**
+* @ClassName: Test  
+* @author Captain Zhang   
+* @date 2016年9月18日 下午8:42:54
+* @version V1.0
+* 
+* @Description: TODO 
+*/
+public class Test {
+
+	/**
+	* @Title: main
+	* @Description: TODO(这里用一句话描述这个方法的作用)
+	* @param @param args    设定文件
+	* @return void    返回类型
+	* @throws
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(17 % 8);
+		
+		String string = "asdfgfd";
+		System.out.println(string.substring(0, 3) + "          " + string);
+	}
+
+}
