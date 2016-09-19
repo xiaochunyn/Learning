6b7b140911b9871a5cbe91055547726d6368fa8d
xiaochunyn <1481839020@qq.com>
2016-09-19 22:16:57 +0800

    update HexConvert.java

--- Learning/src/main/java/zzc/chun/zju/Learning/number/HexConvert.java
+++ Learning/src/main/java/zzc/chun/zju/Learning/number/HexConvert.java
@@ -20,17 +20,33 @@ import java.util.Scanner;
 *	0xA
 *	
 *	输出例子:
-*	10
+*	10	
 *
-*
-*
-*/
+* 定义
+*	16进制即逢16进1，其中用A，B，C，D，E，F（字母不区分大小写）这六个字母来分别表示10，11，12，13，14，15。故而有16进制每一位上可以是从小到大为0、1、2、3、4、5、6、7、8、9、A、B、C、D、E、F共16个大小不同的数。
+*	转换
+*	编辑
+*	16进制到十进制
+*	16进制数的第0位的权值为16的0次方，第1位的权值为16的1次方，第2位的权值为16的2次方……
+*	所以，在第N（N从0开始）位上，如果是是数 X （X 大于等于0，并且X小于等于 15，即：F）表示的大小为 X * 16的N次方。
+*	例：2AF5换算成10进制:
+*	用竖式计算：
+*	第0位： 5 * 16^0 = 5
+*	第1位： F * 16^1 = 240
+*	第2位： A * 16^2= 2560
+*	第3位： 2 * 16^3 = 8192
+*	-------------------------------------
+*	10997
+*	直接计算就是：
+*	5 * 16^0 + F * 16^1 + A * 16^2 + 2 * 16^3 = 10997
+**/
 public class HexConvert {
 	public static void main(String[] args){
 
 //				
 //		char a = 'A';
-//		int b = a;
+//		char aa = '0';
+//		int b = aa;
 //		System.out.println(b);
 //		
 //		System.out.println((char)(97));
@@ -38,34 +54,37 @@ public class HexConvert {
 		Scanner sc = new Scanner(System.in);
 		
 		//去掉0x前面两位
-		String a = sc.nextLine().substring(2);
-		
+		String a ;
+		while(sc.hasNext()){
+			a = sc.nextLine().substring(2);
+			System.out.println(convert(a));
+		}
+				
 		sc.close();
-		
-		System.out.println(convert(a));
-		
+						
 	}
 	
 	public static int convert(String s){
 	
 		int sum = 0;
-		int val = 0;
-		char tmp;
-		
+
+		char tmp;		
 		 
-		for(int i = s.length() - 1; i >= 0; i--){
+		for(int i = 0; i < s.length(); i++){
+			int val = 0;
 			tmp = s.charAt(i);
 			if(tmp >= '0' && tmp <= '9'){
-				val = tmp;
-			}
-			if(tmp >= 'A' && tmp <= 'F'){
+				//特别注意，这里的tmp值为tmp的ACSII值，而非数字之值 ，如 ‘0’ 的ASCII为48
+				val = tmp - '0';
+			}else if(tmp >= 'A' && tmp <= 'F'){
 				val = tmp - 'A' + 10;
-			}
-			if(tmp >= 'a' && tmp <= 'f'){
+			}else if(tmp >= 'a' && tmp <= 'f'){
 				val = tmp - 'a' + 10;
+			}else {
+				break;
 			}
 			
-			sum += val * Math.pow(16, i-1);
+			sum += val * Math.pow(16, s.length() - i - 1);
 			
 		}
 		return sum;
