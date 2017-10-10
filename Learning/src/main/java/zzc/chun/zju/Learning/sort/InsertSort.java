693d87b546cee1a1ede49c58b4d23c644a7db7e3
xiaochunyn <1481839020@qq.com>
2017-10-10 16:09:08 +0800

    add a insert sort example

--- Learning/src/main/java/zzc/chun/zju/Learning/sort/InsertSort.java
+++ Learning/src/main/java/zzc/chun/zju/Learning/sort/InsertSort.java
@@ -2,12 +2,21 @@ package zzc.chun.zju.Learning.sort;
 
 /**
  * Created by chun on 17-3-23.
+ * 基本思想：在要排序的一组数中，假设前面(n-1)[n>=2] 个数已经是排
+ * 好顺序的，现在要把第n个数插到前面的有序数中，使得这n个数
+ * 也是排好顺序的。如此反复循环，直到全部排好顺序。
+ * eg: [1 4 2 3]
+ *
+ * first loop:  i = 1, j = 0; 4 > 1, 不用交换
+ * second loop: i = 2; j = 1; 2 < 4, a[j + 1] = a[j] 即 a[2] = 4
+ *              a[j + 1] = a[j],即a[1] = 2; 此时数组为： [ 1 2 4 3 ]
+ *
+ * third loop： i = 3； j = 2; 3 < 4, a[j + 1] = a[j] 即 a[3] = 4
+ *              a[j + 1] = tmp, 即 a[2] = 3; 此时数组为： [1 2 3 4]
  */
 public class InsertSort {
     public static void main(String[] args){
-        System.out.println("IK");
         int[] a = new int[]{1, 4, 2, 3};
-
         insertSort(a);
         System.out.println("*********************************");
         for(int i = 0; i < a.length; i++){
@@ -16,20 +25,15 @@ public class InsertSort {
         System.out.println("*********************************");
     }
 
-    private static int[] insertSort(int a[]){
-        for(int i = 1; i < a.length; i ++){
+    private static void insertSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
             int tmp = a[i];
             int j = i - 1;
-            for(; j >= 0 && tmp < a[j]; j--){
+            for (; j >= 0 && tmp < a[j]; j--) {
                 a[j + 1] = a[j];
             }
             a[j + 1] = tmp;
         }
-
-        for(int i = 0; i < a.length; i++){
-            System.out.println("Sorted:  " + a[i]);
-        }
-
-        return a;
     }
+
 }
