553a480ec9e147db6c9aebee423a86f98951cbca
xiaochunyn <1481839020@qq.com>
2017-10-17 15:33:21 +0800

    add a new method and testcase

--- Learning/src/main/java/zzc/chun/zju/Learning/algorithm/array/ThreeSum.java
+++ Learning/src/main/java/zzc/chun/zju/Learning/algorithm/array/ThreeSum.java
@@ -53,11 +53,47 @@ public class ThreeSum {
         return res;
     }
 
+    public List<List<Integer>> method2 (int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i ++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int a = nums[i];
+            int j = i + 1, k = nums.length - 1;
+            while (j < k) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    j++;
+                    continue;
+                }
+                int b = nums[j], c = nums[k];
+                int sum = a + b + c;
+                if (sum == 0) {
+                    res.add(new ArrayList<Integer>(){{add(a); add(b); add(c);}});
+                    j++;
+                    k--;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return res;
+    }
+
     public static void main(String []args) {
         int a[] = new int[]{-1, 0, 1, 2, -1, -4};
         System.out.println(new ThreeSum().threeSum(a));
 
         int b[] = new int[]{-8, -2, -1, 0, 1, 2, 3, 6, 7, 8};
         System.out.println(new ThreeSum().threeSum(b));
+
+        int c[] = new int[]{-2,0,0,2,2};
+
+        System.out.println(new ThreeSum().method2(a));
+        System.out.println(new ThreeSum().method2(b));
+        System.out.println(new ThreeSum().method2(c));
     }
 }
